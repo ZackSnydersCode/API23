6a5a5652e7c6be1de62285e06b1b853edcb32746
Pranav Kshirsagar <128245885+ZackSnydersCode@users.noreply.github.com>
2024-05-21 03:23:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ model = genai.GenerativeModel("gemini-pro")
 chat = model.start_chat(history=[])
 @app.route('/')
 def raw_root():
-    return "Done----> code 111 null of status error as a code 404"
+    return "<style>body{backgroundColor:black;color:green;}</style>Done----> code 111 null of status error as a code 404"
 @app.route('/GoogleGenAI',methods=['GET'])
 def dusky():
     return content
