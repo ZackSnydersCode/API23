259cfa793889e3afce036531f4510457866ce760
Pranav Kshirsagar <128245885+ZackSnydersCode@users.noreply.github.com>
2024-05-21 03:59:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,15 +20,16 @@ model = genai.GenerativeModel("gemini-pro")
 chat = model.start_chat(history=[])
 @app.route('/GoogleGenAI',methods=['GET'])
 def dusky():
-    return content
+    return zebronica()
 def zebronica():
     query = readQueries()
     iterator = random.randint(1,len(query))
     question = query[iterator]['q']
     response = chat.send_message(question)
     vision = getVision(query[iterator]['c'])
-    content = jsonify({'blog':response.text,'img':vision})
-
+    content = jsonify({'blog':"Soothes Nausea and Digestion: Ginger is a champion when it comes to calming nausea and indigestion. It can help ease stomach upset, vomiting, and even motion sickness. Studies suggest ginger may speed up the emptying of your stomach, further aiding digestion.",'img':'https://images.pexels.com/photos/3692609/pexels-photo-3692609.jpeg'})
+    return content
+    
 def readQueries():
     try:
         with open('queries.json','r') as q:
